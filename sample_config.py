84f1d16bfc1eed5f5f30c7a09143192c37d45368
Zelzal-SC <97369627+Zelzal-SC@users.noreply.github.com>
2024-06-28 15:37:11 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     # get this value from http://www.timezoneconverter.com/cgi-bin/findzone.tzc
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
-    UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/ZThon-Ar/ZTZ")
+    UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/Zelzal-SC/ZTZ")
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
     VCMODE = os.environ.get("VCMODE", False)
